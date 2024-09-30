2904a17927868b42c7acc492acc92e45120f45f6
Anna Shekshueva <annahex89@gmail.com>
2024-09-30 23:32:37 +0300

    добавлен тест на неверный пароль

--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 
 
 class TestRegisterPage:
@@ -30,6 +31,24 @@ class TestRegisterPage:
         WebDriverWait(driver, 3).until(
             expected_conditions.url_to_be(base_url))
         assert driver.current_url == base_url
+        driver.quit()
+
+    def test_wrong_password(self, register_url, random_email):
+        wrong_password = "1"
+        driver = webdriver.Chrome()
+        driver.get(register_url)
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
+        for input_element in driver.find_elements(By.XPATH, ".//input[@name='name']"):
+            input_element.send_keys(random_email)
+        driver.find_element(By.XPATH, ".//input[@name='Пароль']").send_keys(wrong_password)
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
+        driver.find_element(By.XPATH, ".//form/button").click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located(
+                (By.XPATH, ".//fieldset//p[text()='Некорректный пароль']")))
+        assert driver.current_url == register_url
         driver.quit()
 
 
