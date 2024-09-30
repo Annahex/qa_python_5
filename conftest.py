2d42bb4eaa7c5cf7d79540811e37e920ade3f7c2
Anna Shekshueva <annahex89@gmail.com>
2024-10-01 00:05:59 +0300

    добавлен тест на переход в ЛК

--- conftest.py
+++ conftest.py
@@ -3,6 +3,9 @@ import random
 
 BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 
+__last_generated_email = None
+__last_generated_password = None
+
 
 @pytest.fixture()
 def register_url():
@@ -20,15 +23,36 @@ def forgot_password_url():
 
 
 @pytest.fixture()
+def account_url():
+    return f'{BASE_URL}account/profile'
+
+
+@pytest.fixture()
 def base_url():
     return BASE_URL
 
 
 @pytest.fixture()
 def random_email():
-    return f'anna_shekshueva_11_{random.randint(10000, 99999)}@gmail.com'
+    global __last_generated_email
+    __last_generated_email = f'anna_shekshueva_11_{random.randint(10000, 99999)}@gmail.com'
+    return __last_generated_email
 
 
 @pytest.fixture()
 def random_password():
-    return f'pass{random.randint(10000, 99999)}'
+    global __last_generated_password
+    __last_generated_password = f'pass{random.randint(10000, 99999)}'
+    return __last_generated_password
+
+
+@pytest.fixture()
+def last_generated_email():
+    global __last_generated_email
+    return __last_generated_email
+
+
+@pytest.fixture()
+def last_generated_password():
+    global __last_generated_password
+    return __last_generated_password
--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -33,14 +33,14 @@ class TestRegisterPage:
         assert driver.current_url == base_url
         driver.quit()
 
-    def test_wrong_password(self, register_url, random_email):
+    def test_wrong_password(self, register_url, last_generated_email):
         wrong_password = "1"
         driver = webdriver.Chrome()
         driver.get(register_url)
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
         for input_element in driver.find_elements(By.XPATH, ".//input[@name='name']"):
-            input_element.send_keys(random_email)
+            input_element.send_keys(last_generated_email)
         driver.find_element(By.XPATH, ".//input[@name='Пароль']").send_keys(wrong_password)
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
@@ -99,8 +99,25 @@ class TestRegisterPage:
         assert driver.current_url == login_url
         driver.quit()
 
-
-
-
-
-
+    def test_open_account_page_from_navbar(
+            self, account_url, login_url, base_url, last_generated_password, last_generated_email):
+        driver = webdriver.Chrome()
+        driver.get(login_url)
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
+        driver.find_element(By.XPATH, ".//input[@name='name']").send_keys(last_generated_email)
+        driver.find_element(By.XPATH, ".//input[@name='Пароль']").send_keys(last_generated_password)
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, ".//form//input")))
+        driver.find_element(By.XPATH, ".//form/button").click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.url_to_be(base_url))
+        assert driver.current_url == base_url
+        locator = ".//nav//p[text()='Личный Кабинет']/parent::a"
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, locator)))
+        driver.find_element(By.XPATH, locator).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.url_to_be(account_url))
+        assert driver.current_url == account_url
+        driver.quit()
